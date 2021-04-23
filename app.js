6d9d4feb89a604d5cdfe81c459e4566ff109a7ec
kazunori <gaogaigar673@gmail.com>
2021-04-23 10:38:06 +0900

    [Finish]js 5章

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!</h1>')
+$(function (){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'height': '100px',
+      'width': '200px'
+    }).slideUp();
+  });
 });
